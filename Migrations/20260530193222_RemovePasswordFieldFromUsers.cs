ad8700708142010a211fad2988e3cfe2683b3c7a
nikoladzx <nikolastojkovic320@gmail.com>
2026-05-30 22:54:57 +0200

    removed password from user table

---
+++ Migrations/20260530193222_RemovePasswordFieldFromUsers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace yourmomENT.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePasswordFieldFromUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,6 @@ namespace yourmomENT.Models
     {
         public int Id { get; set; }
         public string? Username { get; set; }
-        public string? Password { get; set; }
         public string? Email { get; set; }
         
         public string? AvatarUrl { get; set; }
